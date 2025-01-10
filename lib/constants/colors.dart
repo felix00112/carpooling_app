b1577ee17c8000137e0233753a0e8a0ca1ec9658
Marike Vossbeck <marike_vossbeck@posteo.de>
2025-01-10 12:53:22 +0100

    colors now as constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color green = Color.fromARGB(106,191,105,1000);
+const Color background_grey = Color.fromARGB(245,245,245,1000);
+const Color background_box_white = Color.fromARGB(255,255,255,1000);
+const Color button_blue = Color.fromARGB(91,118,213,1000);
+
+const Color button_orange = Color.fromARGB(255,166,43,1000);
+const Color button_lightblue = Color.fromARGB(173,187,234,1000);
+
