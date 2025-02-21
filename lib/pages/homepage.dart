1577667da065cf4f9a6c0bb3671edf7c97ac3056
ariamahin <ariamahin@live.de>
2025-02-21 13:55:03 +0100

    feat: -default time -warnings

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,112 +1,120 @@
-import 'package:carpooling_app/pages/fahrt_suchen_aria.dart';
+import 'package:carpooling_app/pages/fahrt_suchen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart'; // Import hinzufügen
 import 'package:carpooling_app/constants/sizes.dart';
 import '../constants/navigationBar.dart'; // Import der NavigationBar
-
 import '../constants/colors.dart';
-
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
-
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
-  ////////////////////Navigation bar/////////////////////
-  int _currentIndex = 0; // Index für die aktuell ausgewählte Seite
-  final Startinputcontroller = TextEditingController();
-  final Zielinputcontroller = TextEditingController();
+  int _currentIndex = 0;
+  final _formKey = GlobalKey<FormState>(); // Form Key für Validierung
+  final TextEditingController Startinputcontroller = TextEditingController();
+  final TextEditingController Zielinputcontroller = TextEditingController();
+  String _selectedOption = "Anbieten";
+  final TextEditingController _dateTimeController = TextEditingController();
 
   void _onTabTapped(int index) {
     setState(() {
       _currentIndex = index;
     });
-    // Navigation zu den entsprechenden Seiten basierend auf dem Index ist nicht mehr notwendig
   }
-/////////////////////////////////////////////////////////
 
-  String _selectedOption = "Anbieten";
-  final TextEditingController _dateTimeController = TextEditingController();
+  void initState() {
+    super.initState();
+    _setDefaultDateTime();
+  }
+
+
+  void _setDefaultDateTime() {
+    DateTime now = DateTime.now();
+    _dateTimeController.text = DateFormat('yyyy-MM-dd – kk:mm').format(now);
+  }
 
   Future<void> _selectDateTime(BuildContext context) async {
-    DateTime? date = DateTime.now();
-    TimeOfDay? time = TimeOfDay.now();
+    DateTime now = DateTime.now(); // Aktuelle Zeit holen
+    DateTime? date;
+    TimeOfDay? time;
 
     final DateTime? pickedDate = await showDatePicker(
       context: context,
-      initialDate: date,
+      initialDate: now,
       firstDate: DateTime(2000),
       lastDate: DateTime(2101),
-      builder: (BuildContext context, Widget? child) {
-        return Theme(
-          data: ThemeData.light().copyWith(
-            primaryColor: button_blue, // Farbe der Kopfzeile
-            colorScheme: ColorScheme.light(
-              primary: button_blue, // Farbe der ausgewählten Zeit
-              secondary: button_blue, // Farbe der Schaltflächen
-            ),
-            buttonTheme: ButtonThemeData(
-              textTheme: ButtonTextTheme.primary, // Farbe des Textes der Schaltflächen
-            ),
-          ),
-          child: child!,
-        );
-      },
     );
 
     if (pickedDate != null) {
       date = pickedDate;
       final TimeOfDay? pickedTime = await showTimePicker(
         context: context,
-        initialTime: time,
-        builder: (BuildContext context, Widget? child) {
-          return Theme(
-            data: ThemeData.light().copyWith(
-              primaryColor: button_blue, // Farbe der Kopfzeile
-              colorScheme: ColorScheme.light(
-                primary: button_blue, // Farbe der ausgewählten Zeit
-                secondary: button_blue, // Farbe der Schaltflächen
-              ),
-              buttonTheme: ButtonThemeData(
-                textTheme: ButtonTextTheme.primary, // Farbe des Textes der Schaltflächen
-              ),
-            ),
-            child: child!,
-          );
-        },        
+        initialTime: TimeOfDay.now(),
       );
 
-      if (pickedTime != null) {
-        time = pickedTime;
-        final DateTime finalDateTime = DateTime(
-          date.year,
-          date.month,
-          date.day,
-          time.hour,
-          time.minute,
+      time = pickedTime ?? TimeOfDay.fromDateTime(now); // Falls keine Uhrzeit gewählt wird, nehme aktuelle Zeit
+    } else {
+      date = now; // Falls kein Datum gewählt wird, nehme aktuelles Datum
+      time = TimeOfDay.fromDateTime(now);
+    }
+
+    final DateTime finalDateTime = DateTime(
+      date!.year,
+      date.month,
+      date.day,
+      time!.hour,
+      time.minute,
+    );
+
+    _dateTimeController.text = DateFormat('yyyy-MM-dd – kk:mm').format(finalDateTime);
+  }
+
+  void _handleStart() {
+    setState(() {
+      if (Startinputcontroller.text.isEmpty) {
+        Startinputcontroller.text = "Bitte ausfüllen!";
+      }
+      if (Zielinputcontroller.text.isEmpty) {
+        Zielinputcontroller.text = "Bitte ausfüllen!";
+      }
+    });
+
+    // Stelle sicher, dass keine "Bitte ausfüllen!"-Werte gesendet werden
+    if (Startinputcontroller.text != "Bitte ausfüllen!" &&
+        Zielinputcontroller.text != "Bitte ausfüllen!") {
+      if (_selectedOption == "Suchen") {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => FindRide(
+              Starteingabe: Startinputcontroller.text,
+              Zieleingabe: Zielinputcontroller.text,
+            ),
+          ),
         );
-        _dateTimeController.text = DateFormat('yyyy-MM-dd – kk:mm').format(finalDateTime);
+      } else if (_selectedOption == "Anbieten") {
+        Navigator.pushNamed(context, '/Anbieten');
       }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    Sizes.initialize(context);
-    return Scaffold(
-      bottomNavigationBar: CustomBottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTabTapped,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
+  Sizes.initialize(context);
+  return Scaffold(
+    bottomNavigationBar: CustomBottomNavigationBar(
+      currentIndex: _currentIndex,
+      onTap: _onTabTapped,
+    ),
+    body: SingleChildScrollView(
+      padding: EdgeInsets.all(Sizes.paddingRegular),
+      child: Form(
+        key: _formKey, // Formular mit Key um Validierung zu ermöglichen
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -116,12 +124,11 @@ class _HomePageState extends State<HomePage> {
                 child: SvgPicture.asset(
                   'assets/images/undraw_city_driver.svg',
                   width: Sizes.deviceWidth,
-                  //height: 700,
                   fit: BoxFit.contain,
                 ),
               ),
             ),
-            SizedBox(height: Sizes.paddingRegular), // platz über überschrift
+            SizedBox(height: Sizes.paddingRegular),
             Text(
               "Wohin möchtest du fahren?",
               style: TextStyle(
@@ -130,132 +137,146 @@ class _HomePageState extends State<HomePage> {
                 color: dark_blue,
               ),
             ),
-
-            SizedBox(height: Sizes.paddingBig), // platz unter überschrift
+            SizedBox(height: Sizes.paddingBig),
             DecoratedBox(
               decoration: BoxDecoration(
                 color: background_box_white,
-                borderRadius: BorderRadius.circular(Sizes.borderRadius)
+                borderRadius: BorderRadius.circular(Sizes.borderRadius),
               ),
               child: Column(
-              children: [
-                SizedBox(height: Sizes.paddingRegular),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: Sizes.paddingSmall),
-                  child: TextField(
-                    controller: Startinputcontroller,
-                    decoration: InputDecoration(
-                      labelText: "Start",
-                      prefixIcon: Icon(Icons.gps_fixed),
-                      border: OutlineInputBorder(),
+                children: [
+                  SizedBox(height: Sizes.paddingRegular),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: Sizes.paddingSmall),
+                    child: TextField(
+                      controller: Startinputcontroller,
+                      decoration: InputDecoration(
+                        labelText: "Start",
+                        prefixIcon: Icon(Icons.gps_fixed),
+                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(
+                          color: Startinputcontroller.text == "Bitte ausfüllen!" ? text_error : dark_blue,
+                        ),
+                      ),
+                      onChanged: (text) {
+                        setState(() {}); // Aktualisiert nur die Farbe des Labels
+                      },
                     ),
                   ),
-                ),
-                SizedBox(height: Sizes.paddingSmall), // platz zwischen den boxen
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: Sizes.paddingSmall),
-                  child: TextField(
-                    controller: Zielinputcontroller,
-                    decoration: InputDecoration(
-                      labelText: "Ziel",
-                      prefixIcon: Icon(Icons.location_on),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                SizedBox(height: Sizes.paddingSmall), // platz zwischen den boxen
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: Sizes.paddingSmall),
-                  child: TextField(
-                    controller: _dateTimeController,
-                    readOnly: true,
-                    decoration: InputDecoration(
-                      labelText: "Zeitpunkt",
-                      prefixIcon: Icon(Icons.calendar_today),
-                      border: OutlineInputBorder(),
-                    ),
-                    onTap: () => _selectDateTime(context),
-                  ),
-                ),
 
-                SizedBox(height: Sizes.paddingSmall), // platz zwischen den Zeitpunkt und anbieten und suchen
-                Row(
-                  children: [
-                    Expanded(
-                      child: RadioListTile(
-                        title: Text("Anbieten",
-                          style: TextStyle(fontSize: Sizes.textTitle, fontWeight: FontWeight.bold),
+                  SizedBox(height: Sizes.paddingSmall),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: Sizes.paddingSmall),
+                    child: TextField(
+                      controller: Zielinputcontroller,
+                      decoration: InputDecoration(
+                        labelText: "Ziel",
+                        prefixIcon: Icon(Icons.location_on),
+                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(
+                          color: Zielinputcontroller.text == "Bitte ausfüllen!" ? text_error : dark_blue,
                         ),
-                        value: "Anbieten",
-                        groupValue: _selectedOption,
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedOption = value.toString();
-                          });
-                        },
                       ),
+                      onChanged: (text) {
+                        setState(() {}); // Aktualisiert nur die Farbe des Labels
+                      },
                     ),
-                    Expanded(
-                      child: RadioListTile(
-                        title: Text("Suchen",
-                          style: TextStyle(fontSize: Sizes.textTitle, fontWeight: FontWeight.bold),
+                  ),
+
+                  SizedBox(height: Sizes.paddingSmall),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: Sizes.paddingSmall),
+                    child: TextField(
+                      controller: _dateTimeController,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        labelText: "Zeitpunkt",
+                        fillColor: dark_blue,
+                        prefixIcon: Icon(Icons.calendar_today),
+                        border: OutlineInputBorder(),
+                      ),
+                      onTap: () => _selectDateTime(context),
+                    ),
+                  ),
+                  SizedBox(height: Sizes.paddingSmall),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RadioListTile(
+                          title: Text(
+                            "Anbieten",
+                            style: TextStyle(
+                              fontSize: Sizes.textTitle,
+                              fontWeight: FontWeight.bold,
+                              color: dark_blue,
+                            ),
+                          ),
+                          value: "Anbieten",
+                          groupValue: _selectedOption,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedOption = value.toString();
+                            });
+                          },
                         ),
-                        value: "Suchen",
-                        groupValue: _selectedOption,
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedOption = value.toString();
-                          });
-                        },
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: Sizes.paddingSmall),
-                Center(
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      if (_selectedOption == "Suchen") {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => FindRide(Starteingabe: Startinputcontroller.text, Zieleingabe: Zielinputcontroller.text,)),
-                        );
-                      } else if (_selectedOption == "Anbieten") {
-                        Navigator.pushNamed(context, '/Anbieten');
-                      }
-                    },
-                    icon: SvgPicture.asset(
-                      'assets/icons/route.svg',
-                      height: 24,
-                      colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                    ), // Verwende SVG-Icon
-                    label: Text(
-                      "Start",
-                      style: TextStyle(
-                        fontSize: Sizes.textSubheading,  // Schriftgröße anpassen
-                        //fontWeight: FontWeight.bold, // Schriftstärke anpassen
-                        color: Colors.white, // Textfarbe anpassen
+                      Expanded(
+                        child: RadioListTile(
+                          title: Text(
+                            "Suchen",
+                            style: TextStyle(
+                              fontSize: Sizes.textTitle,
+                              fontWeight: FontWeight.bold,
+                              color: dark_blue,
+                            ),
+                          ),
+                          value: "Suchen",
+                          groupValue: _selectedOption,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedOption = value.toString();
+                            });
+                          },
+                        ),
                       ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: button_blue,
-                      padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                      minimumSize: Size(double.infinity, 60), // Button länger machen
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(Sizes.borderRadius), // Nur untere Ecken abrunden
-                          bottomRight: Radius.circular(Sizes.borderRadius),
-                        ), // Abgerundete Ecken
+                    ],
+                  ),
+                  SizedBox(height: Sizes.paddingSmall),
+                  Center(
+                    child: ElevatedButton.icon(
+                      onPressed: _handleStart,
+                      icon: SvgPicture.asset(
+                        'assets/icons/route.svg',
+                        height: 24,
+                        colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                      ),
+                      label: Text(
+                        "Start",
+                        style: TextStyle(
+                          fontSize: Sizes.textSubheading,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: button_blue,
+                        padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                        minimumSize: Size(double.infinity, 60),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(Sizes.borderRadius),
+                            bottomRight: Radius.circular(Sizes.borderRadius),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 }
