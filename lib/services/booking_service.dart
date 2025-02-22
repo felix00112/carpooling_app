5082368d78437372f89ca10ec194c1f33888fb2a
Felix <felix-hauptmann@gmx.de>
2025-02-22 21:04:03 +0100

    booking service for create and delete bookings

---
+++ lib/services/booking_service.dart
@@ -0,0 +1,27 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class BookingService{
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  Future<void> createBooking(int rideId) async{
+    final user = _supabase.auth.currentUser;
+    if (user == null) {
+      throw new Exception("User not logged in");
+    }
+
+    await _supabase.from('bookings').insert({
+      'ride_id': rideId,
+      'passenger_id': user.id,
+    });
+  }
+
+  Future<List<Map<String, dynamic>>> getBookingsForRide(int rideId) async{
+    final response = await _supabase
+        .from('bookings')
+        .select()
+        .eq('ride_id', rideId); // only bookings for given ride
+
+    return response ?? [];
+  }
+
+}
