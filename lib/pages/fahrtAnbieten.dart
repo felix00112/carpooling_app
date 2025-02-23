321befaa466423553a4355e8aaef40d41c229084
ariamahin <ariamahin@live.de>
2025-02-23 11:02:26 +0100

    fahrtAnbieten.dart layout changed

--- lib/pages/fahrtAnbieten.dart
+++ lib/pages/fahrtAnbieten.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:carpooling_app/constants/colors.dart';
 import 'package:carpooling_app/constants/textField.dart';
 import 'package:carpooling_app/constants/navigationBar.dart';
+import '../constants/button2.dart';
+import '../constants/sizes.dart';
 
 class OfferRidePage extends StatefulWidget {
   final String Starteingabe;
@@ -21,226 +23,269 @@ class _OfferRidePageState extends State<OfferRidePage> {
   bool _useOwnCar = true;
   final Set<String> _selectedPaymentMethods = {};
 
+  int _currentIndex = 0;
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/home');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/fahrten');
+        break;
+      case 2:
+        Navigator.pushNamed(context, '/profil');
+        break;
+    }
+  }
+
+  @override
   @override
   Widget build(BuildContext context) {
+    Sizes.initialize(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Fahrt anbieten'),
+        title: Text(
+          "Fahrt anbieten",
+          style: TextStyle(
+            fontSize: Sizes.textHeading,
+            fontWeight: FontWeight.bold,
+            color: dark_blue,
+          ),
+        ),
       ),
-      body: SingleChildScrollView(
+      body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: EdgeInsets.only(top: Sizes.paddingRegular), // Hier den Abstand verringern
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start, // Verhindert zu viel vertikalen Abstand
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Mitnahme:', style: TextStyle(fontSize: 16)),
-                  DropdownButton<String>(
-                    value: 'Alle',
-                    items: const [
-                      DropdownMenuItem(value: 'Alle', child: Text('Alle')),
-                      DropdownMenuItem(value: 'Flinta*', child: Text('Nur Flinta*')),
-                    ],
-                    onChanged: (value) {},
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Freie Plätze:', style: TextStyle(fontSize: 16)),
-                  DropdownButton<int>(
-                    value: _freeSeats,
-                    items: List.generate(
-                      7,
-                          (index) => DropdownMenuItem(
-                        value: index + 1,
-                        child: Text('${index + 1}'),
-                      ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        _freeSeats = value!;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Gepäck:', style: TextStyle(fontSize: 16)),
-                  Row(
-                    children: [
-                      Radio<bool>(
-                        value: true,
-                        groupValue: _isLuggageAllowed,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _isLuggageAllowed = value!;
-                          });
-                        },
-                      ),
-                      const Text('Ja'),
-                      Radio<bool>(
-                        value: false,
-                        groupValue: _isLuggageAllowed,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _isLuggageAllowed = value!;
-                          });
-                        },
-                      ),
-                      const Text('Nein'),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Tiermitnahme:', style: TextStyle(fontSize: 16)),
-                  Row(
-                    children: [
-                      Radio<bool>(
-                        value: true,
-                        groupValue: _isPetAllowed,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _isPetAllowed = value!;
-                          });
-                        },
-                      ),
-                      const Text('Ja'),
-                      Radio<bool>(
-                        value: false,
-                        groupValue: _isPetAllowed,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _isPetAllowed = value!;
-                          });
-                        },
-                      ),
-                      const Text('Nein'),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Zwischenstopps:', style: TextStyle(fontSize: 16)),
-                  DropdownButton<int>(
-                    value: _stops,
-                    items: List.generate(
-                      8,
-                          (index) => DropdownMenuItem(
-                        value: index,
-                        child: Text(index == 0 ? 'Keine' : '$index'),
-                      ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        _stops = value!;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Fahrzeug:', style: TextStyle(fontSize: 16)),
-                  Row(
-                    children: [
-                      Radio<bool>(
-                        value: true,
-                        groupValue: _useOwnCar,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _useOwnCar = value!;
-                          });
-                        },
-                      ),
-                      const Text('Eigenes'),
-                      Radio<bool>(
-                        value: false,
-                        groupValue: _useOwnCar,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _useOwnCar = value!;
-                          });
-                        },
-                      ),
-                      const Text('Neues Fahrzeug'),
-                    ],
-                  ),
-                ],
-              ),
-              if (!_useOwnCar)
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    CustomTextField(labelText: 'Kennzeichen', backgroundColor: Colors.grey),
-                    SizedBox(height: 8),
-                    CustomTextField(labelText: 'Autofarbe', backgroundColor: Colors.grey),
-                    SizedBox(height: 8),
-                    CustomTextField(labelText: 'Automodell', backgroundColor: Colors.grey),
-                  ],
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Sizes.borderRadius),
+                  color: Colors.white,
                 ),
-              const SizedBox(height: 16),
-              const Text('Zahlungsmittel:', style: TextStyle(fontSize: 16)),
-              Wrap(
-                spacing: 8.0,
-                children: [
-                  _buildSelectableButton('Bar'),
-                  _buildSelectableButton('PayPal'),
-                  _buildSelectableButton('Kreditkarte'),
-                ],
+                width: MediaQuery.of(context).size.width * 0.925,
+                padding: EdgeInsets.all(Sizes.paddingRegular),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Mitnahme:', style: TextStyle(fontSize: Sizes.textSubheading)),
+                          DropdownButton<String>(
+                            value: 'Alle',
+                            items: const [
+                              DropdownMenuItem(value: 'Alle', child: Text('Alle')),
+                              DropdownMenuItem(value: 'Flinta*', child: Text('Nur Flinta*')),
+                            ],
+                            onChanged: (value) {},
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Freie Plätze:', style: TextStyle(fontSize: Sizes.textSubheading)),
+                          DropdownButton<int>(
+                            value: _freeSeats,
+                            items: List.generate(
+                              7,
+                                  (index) => DropdownMenuItem(
+                                value: index + 1,
+                                child: Text('${index + 1}'),
+                              ),
+                            ),
+                            onChanged: (value) {
+                              setState(() {
+                                _freeSeats = value!;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Gepäck:', style: TextStyle(fontSize: Sizes.textSubheading)),
+                          Row(
+                            children: [
+                              Radio<bool>(
+                                value: true,
+                                groupValue: _isLuggageAllowed,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    _isLuggageAllowed = value!;
+                                  });
+                                },
+                              ),
+                              const Text('Ja'),
+                              Radio<bool>(
+                                value: false,
+                                groupValue: _isLuggageAllowed,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    _isLuggageAllowed = value!;
+                                  });
+                                },
+                              ),
+                              const Text('Nein'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Tiermitnahme:', style: TextStyle(fontSize: Sizes.textSubheading)),
+                          Row(
+                            children: [
+                              Radio<bool>(
+                                value: true,
+                                groupValue: _isPetAllowed,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    _isPetAllowed = value!;
+                                  });
+                                },
+                              ),
+                              const Text('Ja'),
+                              Radio<bool>(
+                                value: false,
+                                groupValue: _isPetAllowed,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    _isPetAllowed = value!;
+                                  });
+                                },
+                              ),
+                              const Text('Nein'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Zwischenstopps:', style: TextStyle(fontSize: Sizes.textSubheading)),
+                          DropdownButton<int>(
+                            value: _stops,
+                            items: List.generate(
+                              8,
+                                  (index) => DropdownMenuItem(
+                                value: index,
+                                child: Text(index == 0 ? 'Keine' : '$index'),
+                              ),
+                            ),
+                            onChanged: (value) {
+                              setState(() {
+                                _stops = value!;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Fahrzeug:', style: TextStyle(fontSize: Sizes.textSubheading)),
+                          Row(
+                            children: [
+                              Radio<bool>(
+                                value: true,
+                                groupValue: _useOwnCar,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    _useOwnCar = value!;
+                                  });
+                                },
+                              ),
+                              const Text('Eigenes'),
+                              Radio<bool>(
+                                value: false,
+                                groupValue: _useOwnCar,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    _useOwnCar = value!;
+                                  });
+                                },
+                              ),
+                              const Text('Neues Fahrzeug'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      if (!_useOwnCar)
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomTextField(labelText: 'Kennzeichen', backgroundColor: Colors.grey),
+                            SizedBox(height: Sizes.paddingSmall),
+                            CustomTextField(labelText: 'Autofarbe', backgroundColor: Colors.grey),
+                            SizedBox(height: Sizes.paddingSmall),
+                            CustomTextField(labelText: 'Automodell', backgroundColor: Colors.grey),
+                          ],
+                        ),
+                      SizedBox(height: Sizes.paddingSmall),
+                      Text('Zahlungsmittel:', style: TextStyle(fontSize: Sizes.textSubheading)),
+                      Wrap(
+                        spacing: Sizes.paddingSmall,
+                        children: [
+                          _buildSelectableButton('Bar'),
+                          _buildSelectableButton('PayPal'),
+                          _buildSelectableButton('Kreditkarte'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              const SizedBox(height: 32), // Platz für den Button unten
+              SizedBox(height: Sizes.paddingRegular),
+              _buildButton(context),
             ],
           ),
         ),
       ),
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+      ),
+    );
+  }
+
+  Widget _buildButton(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: Sizes.paddingSmall),
+      child: Stack(
+        alignment: Alignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: SizedBox(
-              width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>
-                      RideDetailsPage(Starteingabe: widget.Starteingabe,
-                          Zieleingabe: widget.Zieleingabe)),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-
-                backgroundColor: button_orange, // Farbe des Buttons
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              child: const Text(
-                "Fahrt anbieten",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-              ),
-            ),
+          CustomButton2(
+            label: 'Fahrt anbieten',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) =>
+                    RideDetailsPage(Starteingabe: widget.Starteingabe,
+                        Zieleingabe: widget.Zieleingabe)),
+              );
+            },
+            color: button_blue,
+            textColor: Colors.white,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width * 0.925,
+            height: MediaQuery
+                .of(context)
+                .size
+                .width * 0.12,
           ),
-          ),
-          CustomBottomNavigationBar(
-            currentIndex: 0,
-            onTap: (index) {},
-          ),
-
         ],
       ),
     );
@@ -261,7 +306,7 @@ class _OfferRidePageState extends State<OfferRidePage> {
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         decoration: BoxDecoration(
-          color: isSelected ? button_blue : Colors.grey[200],
+          color: isSelected ? dark_blue : Colors.grey[200],
           borderRadius: BorderRadius.circular(8),
         ),
         child: Text(
