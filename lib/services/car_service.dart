ce195317f094dd649e21871f51a94c193278d560
ariamahin <ariamahin@live.de>
2025-02-24 21:36:57 +0100

    add: -getCarsByUser

--- lib/services/car_service.dart
+++ lib/services/car_service.dart
@@ -18,6 +18,40 @@ class CarService{
     });
   }
 
+  Future<List<Map<String, dynamic>>> getCarsByUser(String userId) async {
+    final response = await _supabase
+        .from('cars')
+        .select()
+        .eq('owner', userId); // Filtere die Autos nach der owner-ID
+
+    if (response.isEmpty) {
+      return []; // Keine Autos gefunden
+    }
+
+    return List<Map<String, dynamic>>.from(response);
+  }
+
+
+  Future<int?> getSeats(int rideId) async {
+    try {
+      final response = await _supabase
+          .from('cars')
+          .select('seats') // Nur das Feld 'seats' abrufen
+          .eq('id', rideId)
+          .single();
+
+      if (response != null && response.containsKey('seats')) {
+        return response['seats'] as int; // Die Anzahl der Sitze zurückgeben
+      } else {
+        print("Keine Sitzplatzdaten für Ride $rideId gefunden.");
+        return null;
+      }
+    } catch (e) {
+      print("Fehler beim Abrufen der Sitze für Ride $rideId: $e");
+      return null;
+    }
+  }
+
   Future<List<Map<String, dynamic>>> getCar() async {
     final user = _supabase.auth.currentUser;
     if (user == null) {
@@ -39,5 +73,6 @@ class CarService{
   Future<void> deleteCar(String carId) async {
     await _supabase.from('cars').delete().eq('id', carId);
   }
+
 
 }
