f0a77fdd349f1c0a79a134302acc86ba40b5aef9
Judith <j.bucher@campus.tu-berlin.de>
2025-02-26 22:10:13 +0100

    Fertige Seite mit schon leichter Logik (Fehlermeldung wenn pw zu kurz, pw fehlt oder gar nichts eingegeben wurde)

--- lib/pages/zugangsdaten_ändern.dart
+++ lib/pages/zugangsdaten_ändern.dart
@@ -15,8 +15,14 @@ class ZugangsdatenPage extends StatefulWidget{
 }
 
 class _ZugangsdatenPageState extends State<ZugangsdatenPage>{
-  TextEditingController emailinputcontroller = TextEditingController();
-  TextEditingController passwortinputcontroller = TextEditingController();
+  TextEditingController neuEmailinputcontroller = TextEditingController();
+  TextEditingController altEmailinputcontroller = TextEditingController();
+  TextEditingController neuPasswortinputcontroller = TextEditingController();
+  TextEditingController altPasswortinputcontroller = TextEditingController();
+  TextEditingController neuesPasswortinputcontroller = TextEditingController();
+  bool _showPasswordHint = false;
+  bool _isPassword = false;
+
 
   // Navigation bar index
   int _currentIndex = 2;
@@ -63,61 +69,170 @@ class _ZugangsdatenPageState extends State<ZugangsdatenPage>{
         centerTitle: true,
       ),
 
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              //Mail
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: Sizes.paddingRegular),
-                child: TextField(
-                  controller: emailinputcontroller,
-                  decoration: InputDecoration(
-                    labelText: "alte e-Mail Adresse",
-                    prefixIcon: Icon(FontAwesomeIcons.envelope),
-                    border: OutlineInputBorder(),
+      body: Center(//wenn nicht Mittig verlaget, dann SingleChildScrollView statt Center
+        child: Padding(
+          padding: EdgeInsets.only(top: Sizes.paddingBig, left: Sizes.paddingSmall, right: Sizes.paddingSmall),
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: background_box_white,
+              borderRadius: BorderRadius.circular(Sizes.borderRadius),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              //Inhalt
+              children: [
+                //Überschrift Mail
+                Padding(padding: EdgeInsets.only(top:Sizes.paddingRegular, bottom:Sizes.paddingSmall, left:Sizes.paddingSmall, right:Sizes.paddingSmall),
+                  child: Text("E-Mail Adresse:",
+                    style: TextStyle(
+                        color: dark_blue,
+                        fontSize: Sizes.textSubheading
+                    ),
                   ),
                 ),
-              ),
 
-              //Passwort
-              /*Padding(
-                  padding: EdgeInsets.symmetric(horizontal: Sizes.paddingRegular),
+                //alte Mail
+                Padding(
+                  padding: EdgeInsets.only(left: Sizes.paddingSmall, right: Sizes.paddingSmall, bottom: Sizes.paddingSmall),
                   child: TextField(
-                    controller: passwortinputcontroller,
+                    controller: neuEmailinputcontroller,
+                    decoration: InputDecoration(
+                      labelText: "alte E-Mail Adresse",
+                      prefixIcon: Icon(FontAwesomeIcons.envelope),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                //neue Mail
+                Padding(
+                  padding: EdgeInsets.only(left: Sizes.paddingSmall, right: Sizes.paddingSmall, bottom: Sizes.paddingRegular),
+                  child: TextField(
+                    controller: altEmailinputcontroller,
+                    decoration: InputDecoration(
+                      labelText: "neue E-Mail Adresse",
+                      prefixIcon: Icon(FontAwesomeIcons.envelope),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+
+                //Überschrift Passwort
+                Padding(padding: EdgeInsets.only(top:Sizes.paddingRegular, bottom:Sizes.paddingSmall, left:Sizes.paddingSmall, right:Sizes.paddingSmall),
+                  child: Text("Passwort:",
+                    style: TextStyle(
+                        color: dark_blue,
+                        fontSize: Sizes.textSubheading
+                    ),
+                  ),
+                ),
+
+                //altes Passwort
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: Sizes.paddingSmall),
+                  child: TextField(
+                    controller: altPasswortinputcontroller,
+                    obscureText: true,
                     decoration: InputDecoration(
                       labelText: "altes Passwort",
                       prefixIcon: Icon(FontAwesomeIcons.lock),
                       border: OutlineInputBorder(),
                     ),
                   ),
-              ),*/
+                ),
+                Visibility(
+                  visible: _isPassword,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: Sizes.paddingSmall),
+                    child: Text(
+                      "Bitte geben Sie Ihr Passwort ein!!",
+                      style: TextStyle(color: text_error, fontSize: Sizes.textSubText),
+                    ),
+                  ),
+                ),
 
-              //SpeichernButton
-              Stack(
-                children: [
-                  CustomButton2(
-                    label: 'Speichern',
-                    onPressed: (){
-                      //was soll passieren wenn man auf den speichern klickt
-                      //übergabe/aktualisieren neuer Daten
-                      //zurück zur homePage?
-                      //bestätigungsnachricht?
+                SizedBox(height: Sizes.paddingSmall),
+
+                //neues PW
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: Sizes.paddingSmall),
+                  child: TextField(
+                    controller: neuPasswortinputcontroller,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "neues Passwort",
+                      prefixIcon: Icon(FontAwesomeIcons.key),
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (text) {
+                      setState(() { //kontrollieren ob pw allgemeinen richtlinien passt
+                        _showPasswordHint = text.isNotEmpty && text.length < 8;
+                      });
                     },
-                    color: button_blue,
-                    textColor: Colors.white,
-                    width: MediaQuery
-                        .of(context)
-                        .size
-                        .width * 0.925,
-                    height: MediaQuery
-                        .of(context)
-                        .size
-                        .width * 0.12,
-                  )
-                ],
-              ),
-            ],
+                  ),
+                ),
+                Visibility(
+                  visible: _showPasswordHint,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: Sizes.paddingSmall, right: Sizes.paddingSmall, top: Sizes.paddingSmall),
+                    child: Text(
+                      "Das Passwort muss mindestens 8 Zeichen lang sein!",
+                      style: TextStyle(color: text_error, fontSize: Sizes.textSubText),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: Sizes.paddingRegular),
+
+                //SpeichernButton
+                Padding(padding: EdgeInsets.only(left: Sizes.paddingSmall, right: Sizes.paddingSmall, bottom: Sizes.paddingRegular),
+                  child: Stack(
+                    children: [
+                      CustomButton2(
+                        label: 'Speichern',
+                        onPressed: (){
+                          //was soll passieren wenn man auf den speichern klickt
+                          //übergabe/aktualisieren neuer Daten
+                          //zurück zur homePage?
+                          //bestätigungsnachricht?
+
+                          setState(() {//Überprüfung OB ein PW eingegebn wurde
+                            // Überprüfung, ob das alte Passwort eingegeben wurde
+                            _isPassword = altPasswortinputcontroller.text.isEmpty;
+                          });
+                          if (_isPassword) { // Beende die Funktion bei leerer PW Eingabe, damit die Daten nicht gespeichert werden
+                            return;
+                          }
+
+                          //Schauen ob eine von beiden Sachen geändert wurde
+                          if(neuEmailinputcontroller.text.isEmpty || neuPasswortinputcontroller.text.isEmpty){
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text("Gebe die neuen Daten ein!"))
+                            );
+                          }
+
+                          //Bestätigungsnachricht, dass Daten geändert wurden
+                          ScaffoldMessenger.of(context).showSnackBar(//Bestätigungsnachricht bisher ohne Bedingung
+                              SnackBar(content: Text("Zugangsdaten wurden geändert!"))
+                          );
+
+                        },
+                        color: button_blue,
+                        textColor: Colors.white,
+                        width: MediaQuery
+                            .of(context)
+                            .size
+                            .width * 0.925,
+                        height: MediaQuery
+                            .of(context)
+                            .size
+                            .width * 0.12,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       )
